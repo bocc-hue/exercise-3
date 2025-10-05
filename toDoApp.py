6f83d4ab0c964b7a1d7c9aef067d3b8a59ffd3d0
bocc-hue <boccher13@gmail.com>
2025-10-05 14:13:07 +0800

    Update toDoApp.py

--- toDoApp.py
+++ toDoApp.py
@@ -1,39 +1,45 @@
 # toDoApp.py
 
-tasks=[]
+tasks = []
 
-def addtask(task) :
-  tasks.append(task)
-  print("task added!")
+def addtask(task):
+    tasks.append(task)
+    print("Task added!")
 
-def showTasks( ):
-    if len(tasks)==0 :
-      print("no tasks yet")
+def showTasks():
+    if len(tasks) == 0:
+        print("No tasks yet")
     else:
-     for i in range (len(tasks)):
-      print(i+1,".",tasks[i])
+        for i in range(len(tasks)):
+            print(i+1, ".", tasks[i])
 
 def removetask(tasknumber):
-    tasks.pop(tasknumber) 
-    print("task removed!!")
+    if tasknumber < 1 or tasknumber > len(tasks):
+        print("Invalid task number!")
+    else:
+        tasks.pop(tasknumber - 1)
+        print("Task removed!")
 
 def main():
     while True:
-        print("1 Add Task")
-        print("2.Show Tasks")
-        print("3.Remove Task")
-        print("4- Exit")
-        ch = input("enter choice : ")
-        if ch=="1":
-            t = input("enter task : ")
+        print("1. Add Task")
+        print("2. Show Tasks")
+        print("3. Remove Task")
+        print("4. Exit")
+        ch = input("Enter choice: ")
+        if ch == "1":
+            t = input("Enter task: ")
             addtask(t)
-        elif ch=="2":
+        elif ch == "2":
             showTasks()
-        elif ch=="3":
-            n=int(input("enter task no to remove: "))
-            removetask(n)   
-        elif ch=="4":
-            break;
+        elif ch == "3":
+            try:
+                n = int(input("Enter task number to remove: "))
+                removetask(n)
+            except ValueError:
+                print("Invalid input! Please enter a number.")
+        elif ch == "4":
+            break
         else:
-            print("wrong choice!!")
+            print("Wrong choice!")
 main()
